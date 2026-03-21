5c898b3da3e2174c7c74e04f032d35d4817753c9
Demorck <57636452+Demorck@users.noreply.github.com>
2026-03-21 19:30:55 +0100

    DirectoryPartitionBuilder not working but i'm on it don't worry

--- examples/create_from_dir.py
+++ examples/create_from_dir.py
@@ -1,10 +1,15 @@
 from builder.DirectoryPartitionBuilder import DirectoryPartitionBuilder
+from builder.WiiDiscBuilder import WiiDiscBuilder
 from helpers.Enums import WiiPartType
 
 def main():
     dir_builder = DirectoryPartitionBuilder("src", WiiPartType.DATA)
-    for entry in dir_builder.fst.entries:
-        print(entry.name)
+    with open("../assets/copied_dir.iso", "w+b") as dest:
+        builder = WiiDiscBuilder(dir_builder.encrypted_header, dir_builder.region)
+        builder.add_partition(dest, dir_builder, None)
+
+        builder.finish(dest)
+
 
 if __name__ == "__main__":
     main()
--- src/builder/DirectoryPartitionBuilder.py
+++ src/builder/DirectoryPartitionBuilder.py
@@ -15,27 +15,30 @@ from structs.WiiPartitionEntry import WiiPartitionEntry
 from structs.WiiPartitionHeader import WiiPartitionHeader
 
 
+from file_system_table.FSTNode import FSTFile, FSTDirectory
+
 def build_from_directory_tree(files_dir: str) -> FST:
     fst = FST()
-    dirs = []
-    build_from_directory_tree_recursive(files_dir, dirs, fst)
+    _build_from_directory_tree_recursive(files_dir, fst.entries)
     return fst
 
-def build_from_directory_tree_recursive(path: str, dirs: List[str], fst: FST) -> None:
-    for entry in os.scandir(path):
+def _build_from_directory_tree_recursive(path: str, current_entries: list) -> None:
+    # Ordered
+    entries = sorted(os.scandir(path), key=lambda e: e.name.lower())
+    for entry in entries:
         filename = entry.name
         if entry.is_dir():
-            dirs.append(filename)
-            build_from_directory_tree_recursive(entry.path, dirs, fst)
-            dirs.pop()
+            fst_dir = FSTDirectory(filename)
+            current_entries.append(fst_dir)
+            _build_from_directory_tree_recursive(entry.path, fst_dir.children)
         else:
             fst_file = FSTFile(filename, 0, os.stat(entry).st_size)
-            fst.entries.append(fst_file)
+            current_entries.append(fst_file)
 
 class DirectoryPartitionBuilder(WiiPartitionInterface):
     def __init__(self, path: str, partition_type: WiiPartType)-> None:
         sys_folder = path + "/sys"
-        files_dir = path + "/files"
+        self.files_dir = path + "/files"
         with open(sys_folder + "/boot.bin", 'rb') as f:
             self.encrypted_header = DiscHeader.read(f)
 
@@ -48,9 +51,8 @@ class DirectoryPartitionBuilder(WiiPartitionInterface):
         with open(sys_folder + "/main.dol", 'rb') as f:
             self.dol = f.read()
 
-        # with open(path + "/header.bin", 'rb') as f:
-        #     self.header = WiiPartitionHeader.read(f)
-        #
+        with open(path + "/header.bin", 'rb') as f:
+            self.header = WiiPartitionHeader.read(f)
 
         with open(path + "/tmd.bin", 'rb') as f:
             self.tmd = TMD.read(f)
@@ -60,7 +62,10 @@ class DirectoryPartitionBuilder(WiiPartitionInterface):
             for _ in range(3):
                 self.certificates.append(Certificate.read(f))
 
-        self.fst = build_from_directory_tree(files_dir)
+        # For now
+        self.region: bytes = b'\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x80\x06\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80\x80'
+
+        self.fst = build_from_directory_tree(self.files_dir)
         self.fst_to_bytes = FSTToBytes(self.fst.entries)
         self.partition_type = partition_type
 
@@ -71,7 +76,7 @@ class DirectoryPartitionBuilder(WiiPartitionInterface):
         return self.header
 
     def get_tmd(self) -> TMD:
-        pass
+        return self.tmd
 
     def get_certificates(self) -> List[Certificate]:
         return self.certificates
@@ -92,4 +97,34 @@ class DirectoryPartitionBuilder(WiiPartitionInterface):
         return self.fst_to_bytes
 
     def write_file_data(self, writer: CryptPartWriter, progress_cb: Callable) -> int:
-        pass
+        total_file_count = self.fst_to_bytes.get_total_file_count()
+        file_count = 0
+
+        fst_start_offset = writer.tell() - self.fst_to_bytes.byte_size()
+
+        files = []
+        self.fst_to_bytes.callback_all_files(lambda paths, curr: files.append((paths, curr)))
+        buffer = bytearray()
+        for paths, node in files:
+            # Rebuild path
+            rel_path = os.path.join(*(paths + [node.name]))
+            file_path = os.path.join(self.files_dir, rel_path)
+            
+            with open(file_path, 'rb') as f:
+                node.offset = writer.tell()
+                data = f.read()
+                node.length = len(data)
+                writer.write(data)
+                
+            file_count += 1
+
+
+            if total_file_count > 0 and progress_cb is not None:
+                progress_cb(int(file_count * 100 / total_file_count))
+
+
+        end_of_files_offset = writer.tell()
+        writer.seek(fst_start_offset)
+        self.fst_to_bytes.write_to(writer)
+        writer.seek(end_of_files_offset)
+        return file_count
