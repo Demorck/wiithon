ca0c7eee086b24517d22092222c1ced12a4489f7
Demorck <maximilien.antoine2@gmail.com>
2026-03-30 15:18:40 +0200

    Adding yaz0 (but without testing, i'm at uni lmao, i'll test it tonight) It will probably not work but who cares ?

---
+++ src/file_helper/yaz0.py
@@ -0,0 +1,59 @@
+from io import BytesIO
+from typing import BinaryIO
+
+from helpers.Utils import read_string, read_u32, read_u8
+
+
+class Yaz0:
+    def __init__(self):
+        self.size: int = 0
+        self.magic_word: str = ""
+        self.data: bytes = b""
+
+    @classmethod
+    def read(cls, stream: BinaryIO) -> "Yaz0":
+        obj = cls()
+
+        obj.magic_word = read_string(stream, 0x04)
+        if obj.magic_word != "Yaz0":
+            raise ValueError("Trying to read a non-yaz0 file with the yaz0 struct");
+
+        obj.size = read_u32(stream)
+        stream.read(0x8)
+
+        compressed_data: bytes = stream.read(obj.size)
+        obj.data = Yaz0.uncompress(compressed_data, obj.size)
+
+        return obj
+
+    @staticmethod
+    def uncompress(compressed_data: bytes, size: int) -> bytes:
+        dest_buffer = bytearray()
+        src_buffer = BytesIO(compressed_data)
+        src_buffer.write(compressed_data)
+
+        while len(dest_buffer) < size:
+            group_header = src_buffer.read(0x8)
+            for i in range(8):
+                current_operation = group_header[i]
+                if current_operation & 0x80:
+                    dest_buffer += read_u8(src_buffer)
+                else:
+                    byte1 = read_u8(src_buffer)
+                    byte2 = read_u8(src_buffer)
+
+                    copy_src = len(dest_buffer) - ((byte1 & 0xF) << 8) | byte2 - 1
+                    number_to_copy = byte1 >> 4
+                    if number_to_copy == 0:
+                        number_to_copy = read_u8(src_buffer) + 0x12
+                    else:
+                        number_to_copy += 2
+
+                    if number_to_copy < 3 or number_to_copy > 0x111:
+                        raise ValueError("Something happens when decompressing yaz0 file")
+
+                    for j in range(number_to_copy):
+                        dest_buffer += dest_buffer[copy_src]
+                        copy_src += 1
+
+        return bytes(dest_buffer)
